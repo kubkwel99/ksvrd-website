dd80f26c915eed31d75d325cd5409ed20e2855ba
Adrián Kónya <129780397+kubkwel99@users.noreply.github.com>
2024-06-16 21:27:41 +0200

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -3,17 +3,6 @@ import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-const express = require('express')
-const app = express()
-const port = process.env.PORT || 4000;
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
