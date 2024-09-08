a8504c8b978dd7348a00fc50882007636fc81eb7
Adrián Kónya <129780397+kubkwel99@users.noreply.github.com>
2024-09-08 21:38:48 +0200

    Update portfolio.tsx

--- src/pages/portfolio.tsx
+++ src/pages/portfolio.tsx
@@ -1,3 +1,225 @@
+// 'use client';
+
+// import { motion } from 'framer-motion';
+// import { useEffect, useRef, useState } from 'react';
+// import { FaFacebook, FaInstagram } from 'react-icons/fa';
+// import { FaTiktok } from 'react-icons/fa6';
+// import { IoIosCloseCircleOutline } from 'react-icons/io';
+// import { IoCloseCircleSharp } from 'react-icons/io5';
+// import { headerVariants } from './../../types/motion';
+// import { GetServerSideProps } from 'next';
+// import { getAllMedia } from './../../utils/cloudinary';
+
+// type MediaItem = {
+//   url: string;
+//   public_id: string;
+//   secure_url: string;
+//   resource_type: string;
+//   title: string;
+//   thumbnail_url?: string;
+// };
+
+// const PortfolioPage = () => {
+//   const [media, setMedia] = useState<MediaItem[]>([]);
+//   const [selectedVideo, setSelectedVideo] = useState<string | null>(null);
+//   const [isHovered, setIsHovered] = useState(false);
+//   const [error, setError] = useState<string | null>(null);
+//   const videoRefs = useRef<Map<string, HTMLVideoElement | null>>(new Map());
+
+//   useEffect(() => {
+//     const fetchMedia = async () => {
+//       try {
+//         const response = await fetch('/api/media');
+//         if (!response.ok) {
+//           throw new Error('Network response was not ok');
+//         }
+//         const data = await response.json();
+//         setMedia(data);
+//       } catch (error) {
+//         if (error instanceof Error) {
+//           setError(error.message);
+//           console.error('Error fetching media:', error);
+//         }
+//       }
+//     };
+
+//     fetchMedia();
+//   }, []);
+
+//   useEffect(() => {
+//     const handleKeyDown = (event: KeyboardEvent) => {
+//       if (event.key === 'Escape') {
+//         setSelectedVideo(null);
+//       }
+//     };
+
+//     if (selectedVideo) {
+//       window.addEventListener('keydown', handleKeyDown);
+//     } else {
+//       window.removeEventListener('keydown', handleKeyDown);
+//     }
+
+//     return () => {
+//       window.removeEventListener('keydown', handleKeyDown);
+//     };
+//   }, [selectedVideo]);
+
+//   const handleVideoClick = (url: string) => {
+//     setSelectedVideo(url);
+//   };
+
+//   const closeOverlay = () => {
+//     setSelectedVideo(null);
+//   };
+
+//   useEffect(() => {
+//     const observer = new IntersectionObserver(
+//       (entries) => {
+//         entries.forEach((entry) => {
+//           if (entry.isIntersecting) {
+//             const video = entry.target as HTMLVideoElement;
+//             video.src = video.dataset.src!;
+//             observer.unobserve(video);
+//           }
+//         });
+//       },
+//       {
+//         threshold: 0.1,
+//       }
+//     );
+
+//     media.forEach((item) => {
+//       const video = videoRefs.current.get(item.public_id);
+//       if (video) {
+//         observer.observe(video);
+//       }
+//     });
+
+//     return () => {
+//       media.forEach((item) => {
+//         const video = videoRefs.current.get(item.public_id);
+//         if (video) {
+//           observer.unobserve(video);
+//         }
+//       });
+//     };
+//   }, [media]);
+
+// ;
+//   return (
+//     <motion.div
+//       className='bg-neutral-900 flex flex-col items-center justify-center w-full py-20'
+//       id='portfolio'
+//       style={{
+//         boxShadow: 'inset 0 20px 9px -6px black',
+//       }}>
+//       <h1 className='pb-20 text-3xl'>Portfólio</h1>
+//       <motion.div
+//         variants={headerVariants}
+//         initial='hidden'
+//         whileInView='show'
+//         className='container flex px-4'>
+//         {error ? (
+//           <div className='text-red-500'>{`Error: ${error}`}</div>
+//         ) : (
+//           <div className='grid grid-cols-2 md:grid-cols-3 gap-4 place-items-center m-auto'>
+//             {media.map((item) => (
+//               <div
+//                 className='flex flex-row'
+//                 key={item.public_id}>
+//                 {item.resource_type === 'video' ? (
+//                   <video
+//                     className='cursor-pointer w-64 h-auto aspect-square object-cover'
+//                     width='300px'
+//                     data-src={item.secure_url}
+//                   aria-placeholder='img'
+//                     onClick={() => handleVideoClick(item.secure_url)}
+//                     poster={item.thumbnail_url} // Use the thumbnail URL for the video poster
+//                   >
+//                     Your browser does not support the video tag.
+//                   </video>
+//                 ) : (
+//                   <img
+//                     src={item.secure_url}
+//                     alt={item.title}
+//                     className='cursor-pointer w-52 h-auto object-cover'
+
+//                     onClick={() => {}}
+//                   />
+//                 )}
+//               </div>
+//             ))}
+//           </div>
+//         )}
+
+//         {selectedVideo && (
+//           <div
+//             className='fixed top-0 right-0 flex flex-col m-0 w-full h-full backdrop-blur-md'
+//             onClick={closeOverlay}>
+//             <div className='flex flex-col items-center justify-center max-h-screen py-40 relative'>
+//               <video
+//                 className='max-w-full max-h-full rounded-lg'
+//                 controls
+//                 autoPlay>
+//                 <source
+//                   src={selectedVideo}
+//                   type='video/mp4'
+//                 />
+//                 Your browser does not support the video tag.
+//               </video>
+//               <div className='p-4'>
+//                 <ul className='flex gap-2 text-2xl items-center'>
+//                   <li className='hover:scale-110 transition ease-in-out'>
+//                     <a
+//                       href='https://www.facebook.com/kika.svoradova?locale=sk_SK'
+//                       target='_blank'
+//                       rel='noopener noreferrer'>
+//                       <FaFacebook />
+//                     </a>
+//                   </li>
+//                   <li className='hover:scale-110 transition ease-in-out'>
+//                     <a
+//                       href='https://www.instagram.com/svoradova.k/'
+//                       target='_blank'
+//                       rel='noopener noreferrer'>
+//                       <FaInstagram />
+//                     </a>
+//                   </li>
+//                   <li className='hover:scale-110 transition ease-in-out'>
+//                     <a
+//                       href='https://www.tiktok.com/@krsvrd'
+//                       target='_blank'
+//                       rel='noopener noreferrer'>
+//                       <FaTiktok />
+//                     </a>
+//                   </li>
+//                 </ul>
+//               </div>
+//               <button
+//                 className='absolute right-0 top-24 mx-10 py-5 text-3xl text-black rounded hover:transition ease-in-out delay-100'
+//                 onMouseEnter={() => setIsHovered(true)}
+//                 onMouseLeave={() => setIsHovered(false)}
+//                 onClick={closeOverlay}>
+//                 {isHovered ? <IoCloseCircleSharp /> : <IoIosCloseCircleOutline />}
+//               </button>
+//             </div>
+//           </div>
+//         )}
+//       </motion.div>
+//     </motion.div>
+//   );
+// };
+
+// export const getServerSideProps: GetServerSideProps = async () => {
+//   const media = await getAllMedia(); // Ensure this returns the correct data
+//   return {
+//     props: {
+//       media,
+//     },
+//   };
+// };
+
+// export default PortfolioPage;
 'use client';
 
 import { motion } from 'framer-motion';
@@ -85,6 +307,9 @@ const PortfolioPage = () => {
   const closeOverlay = () => {
     setSelectedVideo(null);
   };
+  const handleVideoClick = (url: string) => {
+    setSelectedVideo(url);
+  };
 
   return (
     <motion.div
@@ -109,7 +334,9 @@ const PortfolioPage = () => {
                 className=''
                 key={video.public_id}>
                 {video.resource_type === 'video' ? (
-                  <div className='cursor-pointer w-64 h-auto aspect-square '>
+                  <div
+                    className='cursor-pointer w-64 h-auto aspect-square'
+                    onClick={() => handleVideoClick(video.url)}>
                     {media && (
                       <CldVideoPlayer
                         className=' rounded-xl aspect-square bg-cover '
@@ -135,6 +362,7 @@ const PortfolioPage = () => {
                     src={video.secure_url}
                     alt={video.title}
                     className='cursor-pointer w-52 h-auto object-cover'
+                    onClick={() => handleVideoClick(video.secure_url)}
                   />
                 )}
               </div>
