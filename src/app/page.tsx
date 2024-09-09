ba85af10b2cdd4f896903e36f5537f51e3bc7d21
Adrián Kónya <129780397+kubkwel99@users.noreply.github.com>
2024-09-09 19:20:12 +0200

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,10 @@ import Footer from './../../components/Footer';
 import AboutPage from './../pages/about';
 import PortfolioPage from './../pages/portfolio';
 import { useState, useEffect } from 'react';
+import Preloader from '../../components/Preloader';
 
 type MediaItem = {
+  id: number;
   url: string;
   public_id: string;
   secure_url: string;
@@ -17,69 +19,85 @@ type MediaItem = {
 
 export default function HomePage() {
   const [media, setMedia] = useState<MediaItem[]>([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000); // Adjust the delay to your needs
+
+    return () => clearTimeout(timer);
+  }, []);
+
   useEffect(() => {
     // Fetch media data from an API or other source
     fetch('/api/media')
-      .then(response => response.json())
-      .then(data => setMedia(data))
-      .catch(error => console.error('Error fetching media:', error));
+      .then((response) => response.json())
+      .then((data) => setMedia(data))
+      .catch((error) => console.error('Error fetching media:', error));
   }, []);
-  return (
-    <div
-      className='bg-top h-screen text-white'
-      style={{
-        background: 'url(/images/bg.jpg)',
-        backgroundSize: 'cover',
-        backgroundRepeat: 'no-repeat',
-        zIndex: '-100',
-      }}>
-      <motion.div
-        variants={headerVariants}
-        initial='hidden'
-        whileInView='show'
-        transition={{
-          duration: 0.1,
-          delay: 0.1,
-          ease: 'easeInOut',
-          loop: Infinity,
-        }}
-        viewport={{ once: false, amount: 0.5 }}
-        className='container p-4 mx-auto flex fle justify-between items-center h-screen'>
-        <motion.div
-          className='flex flex-col'
-          variants={staggerContainer(0.1, 0.9)}
-          initial='hidden'
-          animate='show'
-          viewport={{ once: false, amount: 0.15 }}>
-          <motion.h1
-            variants={textVariant(0.2)}
-            className='text-6xl font-extrabold py-2 font-mono'>
-            Ahoj, som
-          </motion.h1>
-          <motion.p
-            variants={textVariant(0.6)}
-            className='text-2xl font-semibold pb-6 text-red-500'>
-            Tvorca medialného obsahu
-          </motion.p>
 
-          <motion.p
-            variants={textVariant(0.8)}
-            className='w-1/2 min-w-80'>
-            Som kreatívna hlava s množstvom nápadov, ktoré sa snažím pretvoriť do videí. Tvoreniu
-            amatérskych videí sa venujem približne 3 roky a stále ma to neprestalo baviť!
-          </motion.p>
-          <motion.button variants={textVariant(1)} className='bg-black opacity-100 p-2 my-8 w-36 rounded-xl  hover:bg-neutral-950 transition-all'>
-            <a
-              className='hover:opacity-75'
-              href='mailto:info@example.com'>
-              Napíš mi.
-            </a>
-          </motion.button>
+  return (
+    <>
+      {loading && <Preloader />}
+      <div
+        className='bg-top h-screen text-white'
+        style={{
+          background: 'url(/images/bg.jpg)',
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+          zIndex: '-100',
+        }}>
+        <motion.div
+          variants={headerVariants}
+          initial='hidden'
+          whileInView='show'
+          transition={{
+            duration: 0.1,
+            delay: 0.1,
+            ease: 'easeInOut',
+            loop: Infinity,
+          }}
+          viewport={{ once: false, amount: 0.5 }}
+          className='container p-4 mx-auto flex fle justify-between items-center h-screen'>
+          <motion.div
+            className='flex flex-col'
+            variants={staggerContainer(0.1, 0.9)}
+            initial='hidden'
+            animate='show'
+            viewport={{ once: false, amount: 0.15 }}>
+            <motion.h1
+              variants={textVariant(0.2)}
+              className='text-6xl font-extrabold py-2 font-mono'>
+              Ahoj, som Kristína Svoradová.
+            </motion.h1>
+            <motion.p
+              variants={textVariant(0.6)}
+              className='text-2xl font-semibold pb-6 text-red-500'>
+              Tvorca medialného obsahu
+            </motion.p>
+
+            <motion.p
+              variants={textVariant(0.8)}
+              className='w-1/2 min-w-80'>
+              Som kreatívna hlava s množstvom nápadov, ktoré sa snažím pretvoriť do videí. Tvoreniu
+              amatérskych videí sa venujem približne 3 roky a stále ma to neprestalo baviť!
+            </motion.p>
+            <motion.button
+              variants={textVariant(1)}
+              className='bg-black opacity-100 p-2 my-8 w-36 rounded-xl  hover:bg-neutral-950 transition-all'>
+              <a
+                className='hover:opacity-75'
+                href='mailto:info@example.com'>
+                Napíš mi.
+              </a>
+            </motion.button>
+          </motion.div>
         </motion.div>
-      </motion.div>
-      <AboutPage />
-      <PortfolioPage media={media} />
-      <Footer/>
-    </div>
+        <AboutPage />
+        <PortfolioPage media={media} />
+        <Footer />
+      </div>
+    </>
   );
 }
