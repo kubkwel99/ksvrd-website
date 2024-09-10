3d808c9c4e0ec3f45b5653ba1378b533ff381f95
Adrián Kónya <129780397+kubkwel99@users.noreply.github.com>
2024-09-10 17:21:31 +0200

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,13 @@ import { AuthProvider } from './../contexts/AuthContext';
 import Navbar from './../../components/Navbar';
 import './../../styles/globals.css'
 import { Poppins } from 'next/font/google';
-import localFont from 'next/font/local';
 
 const poppins = Poppins({ subsets: ['latin'], weight: ['400', '500', '600', '700', '800', '900'] });
-
-const myFont = localFont({ src: './../../public/FiraSansCondensed-Regular.ttf' });
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='sk'>
-      <body className={myFont.className}>
+      <body className={Poppins.className}>
         <AuthProvider>
           <Navbar />
           <main>{children}</main>
